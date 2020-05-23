51f172fc182f6059c7bc5655cbd6522d4be995c9
Govind <Sumanth.Govind@sprint.com>
2020-05-23 17:43:48 -0400

    initial commit

---
+++ misc.py
@@ -0,0 +1,16 @@
+def double(num):
+    """Function to double the valuessssss"""
+    return 2*num
+print(double.__doc__)
+print(double(6))
+
+
+
+a, b, c = 5, 3.2, "Hello"
+
+print (a)
+print (b)
+print (c)
+
+pi = 3.14
+gravity = 9.8
